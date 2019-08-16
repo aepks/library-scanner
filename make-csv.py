3b64b160c4bffd7141de4b92f7c47456939a86cc
Gabriel Krell <gabrielkrell@gmail.com>
2019-08-16 18:21:02 -0500

    Add ISBN checking, refactor a bit
    Also fix stupid isbn= -> isbn: in the query

--- make-csv.py
+++ make-csv.py
@@ -5,6 +5,25 @@ import json
 
 GOOGLE_API_KEY = ''
 
+def dump_response(r):
+	print(json.dumps(r.json(), indent=2, sort_keys=True))
+
+def matches_isbn(isbn, volumeInfo):
+	if 'industryIdentifiers' not in volumeInfo:
+		print("     no identifiers for {}".format(isbn))
+		return False
+	for identifier in volumeInfo['industryIdentifiers']:
+		try:
+			if int(identifier['identifier']) == isbn:
+				return True
+			# print("id {} != isbn {}".format(identifier['identifier'], isbn))
+		except ValueError as e:
+			pass # ignore non-numeric ISBNs, since our scanner doesn't generate them anyway
+			# print("Found a non-numeric identifier: {}".format(identifier['identifier']))
+	return False
+
+
+
 parser = argparse.ArgumentParser(description="Turn a list of ISBNs into a CSV")
 parser.add_argument("isbns", help="A file with an ISBN on each line", type=argparse.FileType('r'))
 parser.add_argument("output", help="A CSV file with the book details", type=argparse.FileType('w'), default='-')
@@ -30,25 +49,25 @@ for isbn in args.isbns:
 	print("Searching for {}".format(isbn))
 	r = requests.get(
 		'https://www.googleapis.com/books/v1/volumes', params={
-			'q': 'isbn={}'.format(isbn),
-			'fields': 'items/volumeInfo/authors,items/volumeInfo/title,items/id',
+			'q': 'isbn:{}'.format(isbn),
+			'fields': 'items/volumeInfo/authors,items/volumeInfo/title,items/volumeInfo/industryIdentifiers,items/id',
 			'maxResults': 1,
 			'key': GOOGLE_API_KEY
 			})
 	if r.status_code == 403:
 		print("Oop, we might be rate-limited. Stopping. Response:")
-		print(json.dumps(r.json(), indent=2, sort_keys=True))
+		dump_response(r)
 		exit()
 	if r.status_code != 200:
 		print("Uh-oh, got a non-200 return. Continuing to the next one. Response:")
-		print(json.dumps(r.json(), indent=2, sort_keys=True))
+		dump_response(r)
 		continue
 
 	try:
 		book = r.json()['items'][0]
 	except KeyError as e:
 		print("No results for {}!".format(isbn))
-		print(json.dumps(r.json(), indent=2, sort_keys=True))
+		dump_response(r)
 		writer.writerow({
 			'ISBN': isbn,
 			'Comment': comment
@@ -56,6 +75,14 @@ for isbn in args.isbns:
 		continue
 
 	try:
+		if not matches_isbn(isbn, book['volumeInfo']):
+			print("Matching ISBN not found for {}!".format(isbn))
+			writer.writerow({
+				'ISBN': isbn,
+				'Comment': comment
+			})
+			dump_response(r)
+			continue
 		if 'subtitle' in book['volumeInfo']:
 			title = "{title}: {subtitle}".format(**book['volumeInfo'])
 		else:
